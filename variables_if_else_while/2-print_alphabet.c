19455d720cd09a6b63ab02a581bdbb7c7870def5
Pierre338 <8965@holbertonstudents.com>
2024-02-20 16:02:13 +0000

    PutcharAZ

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*
+* main – print alphabet
+* Return: 0
+*/
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar ('\n');
+
+	return (0);
+
+}
