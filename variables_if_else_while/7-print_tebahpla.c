da24f8e12798700374d34552d9a12d9bfe485c78
Pierre338 <8965@holbertonstudents.com>
2024-02-22 16:03:38 +0000

    Mirror alphabet lowercase

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print alphabet in reverse with putchar
+ * Return: 0
+ */
+int main(void)
+{
+	char letter = 'z';
+
+	while (letter <= 'a')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar ('\n');
+
+	return (0);
+}
