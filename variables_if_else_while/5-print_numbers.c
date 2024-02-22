e9747c0665e623a7758ea4e7f6ac55b7cd831a0e
Pierre338 <8965@holbertonstudents.com>
2024-02-22 14:15:43 +0000

    print all digits from 0>10

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Print all digit from 0>10
+ * Return: 0
+ */
+int main(void)
+
+{
+	int n;
+
+
+	if n = 0
+
+	{
+
+		printf("all single digit of <=10")
+
+	return (0);
+
+	}
+
+}
