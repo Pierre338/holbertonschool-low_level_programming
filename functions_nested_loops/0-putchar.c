83c0c5b9c1b8cc432d84f8c7b2c10eba38ea358d
Pierre338 <8965@holbertonstudents.com>
2024-02-23 11:08:09 +0000

    _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -2,11 +2,13 @@
 
 /**
  * main - Print _putchar
+ *
+ * Description: print putchar
+ *
  * Return : 0
  */
 
 int main(void)
-
 {
 	_putchar('_');
 	_putchar('p');
@@ -17,7 +19,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
 
 	return (0);
 }
