c72b6ee012cacaf71fee1be6cb1b16479c81b72b
Pierre338 <8965@holbertonstudents.com>
2024-02-26 14:32:01 +0000

    3putchar

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -10,16 +10,16 @@ void more_numbers(void)
 {
 	int i, n;
 
-		for (i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
+	{
+		for (n = 0; n < 15; n++)
 		{
-			for (n = 0; n < 15; n++)
-				{
-					if (n > 9)
-					{
-						_putchar((n / 10) + '0');
-					}
-					_putchar((n % 10) + '0');
-				}
-		_putchar('\n');
+			if (n > 9)
+			{
+				_putchar((n / 10) + '0');
+			}
+			_putchar((n % 10) + '0');
 		}
+	_putchar('\n');
+	}
 }
