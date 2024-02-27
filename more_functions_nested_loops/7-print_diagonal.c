b2d7c384e700d3032cda45860a250348ba40a511
Pierre338 <8965@holbertonstudents.com>
2024-02-27 08:54:19 +0000

    \

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -8,11 +8,22 @@
 
 void print_diagonal(int n)
 {
-	int i;
-
-	for (i = 0; i < n; i++)
+	int i, space;
+	if (n > 0)
 	{
-		_putchar('\\');
+		for (i = 0; i < n; i++)
+		{
+			for (space = 0; space < i; space++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			if (i == (n - 1))
+			{
+				continue;
+			}
+			_putchar('\n');
+		}
 	}
 	_putchar('\n');
 }
