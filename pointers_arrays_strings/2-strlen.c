862c03712b50dd8b280acbbb0bcdcf96bd481abf
Pierre338 <8965@holbertonstudents.com>
2024-02-28 13:19:47 +0000

    strlen

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -2,11 +2,22 @@
 
 /**
  * _strlen - return length
- *@
+ * b stand for string lengh
+ *@i : string
  * Return: Always 0.
  */
 
-int _strlen(char *s);
+int _strlen(char *s)
 
 {
+	int b = 0;
 
+	while (*s != '\0')
+
+	{
+		b++;
+		s++;
+	}
+
+	return (b);
+}
