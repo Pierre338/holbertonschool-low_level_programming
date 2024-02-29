21a42804dc0e1eae9bd5cdbafaedfca71f55a775
Pierre338 <8965@holbertonstudents.com>
2024-02-29 16:18:03 +0000

    rev

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,10 +1,32 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
-int main(void)
+
+void rev_string(char *s)
+
 {
+	int i, j, k;
+
+	char temp;
+
+	for (i = 0; s[i] != '\0'; i++)
+
+	{
+	}
+
+	k = i - 1;
+
+	for (j = 0; j < i / 2; j++)
+
+	{
+		temp = s[j];
+		s[j] = s[k];
+		s[k] = temp;
+	k--;
+
+	}
+}
