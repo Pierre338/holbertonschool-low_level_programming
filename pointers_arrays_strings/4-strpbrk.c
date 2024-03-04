707bbc634cc5201bde267d21226ad7ae3dff560e
Pierre338 <8965@holbertonstudents.com>
2024-03-04 12:24:50 +0000

    s

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+*_strpbrk - s
+*
+*@s: s
+*@accept: s
+*Return: s
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (accept[i] == *s)
+				return (s);
+		}
+		s++;
+	}
+
+	return ('\0');
+}
