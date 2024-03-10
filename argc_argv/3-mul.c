3e8b688966d885cf3344f36c3aae31f4de26cf1b
Pierre338 <8965@holbertonstudents.com>
2024-03-10 18:21:05 +0000

    mul

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main- m
+ * @argc: n
+ * @argv: a
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, j = 1;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		j *= atoi(argv[i]);
+	}
+	printf("%d\n", j);
+	return (0);
+}
--- argc_argv/main.h
+++ argc_argv/main.h
@@ -7,4 +7,5 @@ int _putchar(char c);
 int main(int argc, char *argv[]);
 int main(int argc, char **argv);
 int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
 #endif
