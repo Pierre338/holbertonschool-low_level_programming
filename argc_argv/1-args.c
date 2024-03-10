ebe8fccf24a691b225db29e4fb9b5a9c212d179e
Pierre338 <8965@holbertonstudents.com>
2024-03-10 18:13:56 +0000

    args

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - r
+ * @argc: c
+ * @argv: p
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	(void) argv;
+	printf("%i\n", argc - 1);
+	return (0);
+}
