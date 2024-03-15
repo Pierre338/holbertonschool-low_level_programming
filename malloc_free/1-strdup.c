5677d554e14393bfb295be9b171ee73bccca3489
Pierre338 <8965@holbertonstudents.com>
2024-03-15 18:41:27 +0000

    s

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* _strdup - r
+*@str: s
+*Return: n
+*/
+
+char *_strdup(char *str)
+{
+	char *cpy;
+	int index, len;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (index = 0; str[index]; index++)
+		len++;
+	cpy = malloc(sizeof(char) * (len + 1));
+
+	if (cpy == NULL)
+		return (NULL);
+
+	for (index = 0; str[index]; index++)
+	{
+		cpy[index] = str[index];
+	}
+
+	cpy[len] = '\0';
+
+	return (cpy);
+
+}
