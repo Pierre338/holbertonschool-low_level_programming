e337f3d9541e7621ce5ea9cf7c16bf106d4cbd2e
Pierre338 <8965@holbertonstudents.com>
2024-03-15 21:14:50 +0000

    array

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,51 +1,42 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
- * string_nconcat - e
+ *string_nconcat - s
  *@s1: s1
  *@s2: s2
  *@n: n
- * Return: p
+ *Return: nu
+ *Otherwise - p
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *strnew = NULL;
-	unsigned int i, n1, n2, j, count, palabras;
+	char *concat;
+	unsigned int len = n, index;
 
-	count = 0;
-	palabras = 0;
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
-	for (n1 = 0; s1[n1] != '\0'; n1++)
-		;
-	for (n2 = 0; s2[n2] != '\0'; n2++)
-		;
-	if (n >= n2)
-	{
-		palabras = n2;
 
-	} else
-	{
-		for (n2 = 0; n2 < n; n2++)
-			palabras++;
-	}
-	strnew = (char *)malloc((n1 + n2 + 1) * sizeof(char));
-	if (strnew == NULL)
-	{
+	for (index = 0; s1[index]; index++)
+		len++;
+
+	concat = malloc(sizeof(char) * (len + 1));
+
+	if (concat == NULL)
 		return (NULL);
-	}
-	for (i = 0; s1[i] != '\0'; i++)
-		strnew[i] = s1[i];
-	for (j = 0; j < palabras; i++)
-	{
-		strnew[i] = s2[count];
-		count++;
-		j++;
-	}
-	strnew[i] = '\0';
-	return (strnew);
+
+	len = 0;
+
+	for (index = 0; s1[index]; index++)
+		concat[len++] = s1[index];
+
+	for (index = 0; s2[index] && index < n; index++)
+		concat[len++] = s2[index];
+
+	concat[len] = '\0';
+
+	return (concat);
 }
